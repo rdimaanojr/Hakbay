493651e149af264b79155dfda32341ae8f5a9235
ashyphrodite <rddimaano2@up.edu.ph>
2025-05-08 00:01:54 +0800

    feat: ADD logging system

---
+++ hakbay/lib/utils/logger.dart
@@ -0,0 +1,13 @@
+import 'package:logger/logger.dart';
+
+final logger = Logger(
+  printer: PrettyPrinter(
+    methodCount: 2, // no. of method calls to be called
+    errorMethodCount: 8, // methods for stacktrace
+    lineLength: 120, // output width
+    colors: true, // colorful messages
+    printEmojis: true, // print emoji for each log message
+    // timestamp each log print
+    dateTimeFormat: DateTimeFormat.onlyTimeAndSinceStart,
+  ),
+);
