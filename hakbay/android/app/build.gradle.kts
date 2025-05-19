2535fda4e4b6409ffe3e7432c33b59acebbed98f
mrbanasihan <mrbanasihan@up.edu.ph>
2025-05-19 20:22:24 +0800

    chore: added dependencies for notification

--- hakbay/android/app/build.gradle.kts
+++ hakbay/android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -44,4 +45,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.22")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
